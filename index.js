be9a457e0fa8dd6c57abb1480a0b67d1dfcd7e88
GoldenPavilion <rcorystine@gmail.com>
2021-08-23 12:35:53 -0400

    completed createFareMultiplier

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const returnFirstTwoDrivers = function(driverArray){
 const returnLastTwoDrivers = function(driverArray){
     return driverArray.slice(-2)
 }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function(multiplier){
+        return int * multiplier
+    }
+}
