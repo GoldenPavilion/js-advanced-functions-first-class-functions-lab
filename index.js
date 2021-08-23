4f59b4fe0cccab0cd458f6539a44d7c4b1812fb1
GoldenPavilion <rcorystine@gmail.com>
2021-08-23 12:26:18 -0400

    returns first and last two drivers

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(driverArray){
+    return driverArray.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(driverArray){
+    return driverArray.slice(-2)
+}
